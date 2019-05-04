41465bb10843f0d4f630fa954720e7e14dadaec1
alessanderleite <alessander.logistica@gmail.com>
2019-05-03 22:50:16 -0300

    code the class Itemresponse, add constructor, getter and setter methods

--- app/src/main/java/br/com/alessanderleite/recyclerviewwithsearchview/model/ItemResponse.java
+++ app/src/main/java/br/com/alessanderleite/recyclerviewwithsearchview/model/ItemResponse.java
@@ -1,4 +1,25 @@
 package br.com.alessanderleite.recyclerviewwithsearchview.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class ItemResponse {
+
+    @SerializedName("items")
+    @Expose
+    private List<Item> itemList;
+
+    public ItemResponse(List<Item> itemList) {
+        this.itemList = itemList;
+    }
+
+    public List<Item> getItemList() {
+        return itemList;
+    }
+
+    public void setItemList(List<Item> itemList) {
+        this.itemList = itemList;
+    }
 }
