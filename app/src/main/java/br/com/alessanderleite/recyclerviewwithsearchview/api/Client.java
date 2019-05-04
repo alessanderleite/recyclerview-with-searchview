fafd503208ac0ff0724111759d9ed8ef096709bc
alessanderleite <alessander.logistica@gmail.com>
2019-05-03 22:55:24 -0300

    code the Client class

--- app/src/main/java/br/com/alessanderleite/recyclerviewwithsearchview/api/Client.java
+++ app/src/main/java/br/com/alessanderleite/recyclerviewwithsearchview/api/Client.java
@@ -1,4 +1,20 @@
 package br.com.alessanderleite.recyclerviewwithsearchview.api;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class Client {
+
+    private static final String BASE_URL = "https://api.github.com/";
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
