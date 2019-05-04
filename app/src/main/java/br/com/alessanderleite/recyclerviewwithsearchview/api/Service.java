921e1bb0b726eb31019c2d8726439af5e604a3f6
alessanderleite <alessander.logistica@gmail.com>
2019-05-03 22:57:00 -0300

    code the interface Service

--- app/src/main/java/br/com/alessanderleite/recyclerviewwithsearchview/api/Service.java
+++ app/src/main/java/br/com/alessanderleite/recyclerviewwithsearchview/api/Service.java
@@ -1,4 +1,11 @@
 package br.com.alessanderleite.recyclerviewwithsearchview.api;
 
+import br.com.alessanderleite.recyclerviewwithsearchview.model.ItemResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface Service {
+
+    @GET("search/users?q=language:java+location:brazil")
+    Call<ItemResponse> getItems();
 }
